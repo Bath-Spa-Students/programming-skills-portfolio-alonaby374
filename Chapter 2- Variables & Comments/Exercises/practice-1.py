84431b9aa89846be279f814b9e707aab9afdd59a
alonaby374 <alonaby374@gmail.com>
2023-11-04 16:20:52 +0400

    Create practice-1.py

---
+++ Chapter 2- Variables & Comments/Exercises/practice-1.py
@@ -0,0 +1,38 @@
+x=("what's")
+y=(" up")
+z=(x + y)
+print(z,end="\t")
+print("My friend")
+print("*****************************************************************")
+
+
+name="Alon"
+print(f"My Name is {name}")
+print()
+print("*****************************************************************")
+
+
+import turtle
+turtle.pensize(15)
+turtle.pencolor('red')
+turtle.circle(150)
+
+import turtle
+turtle.pensize(15)
+turtle.pencolor('black')
+turtle.circle(100)
+
+import turtle
+turtle.pensize(15)
+turtle.pencolor('red')
+turtle.circle(50)
+
+import turtle
+turtle.pensize(15)
+turtle.pencolor('black')
+turtle.circle(30)
+
+import turtle
+turtle.pensize(15)
+turtle.pencolor('red')
+turtle.circle(10)
