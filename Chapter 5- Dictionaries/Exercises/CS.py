d59c7170587a6bd2194b0c0d64f54c7c76034b7f
alonaby374 <alonaby374@gmail.com>
2023-11-04 16:20:33 +0400

    Create CS.py

---
+++ Chapter 5- Dictionaries/Exercises/CS.py
@@ -0,0 +1,12 @@
+# To add something in dictionary
+example={'color':'red',
+         'fruit':'apple',
+         'place':'United arab emirates'}
+print(example)
+
+
+# Lets check if we want to return one value, so it is a dictionary
+dictionary={'Name':'Alon',
+            'Roll No':'1234',
+            'place':'United arab emirates'}
+print(dictionary['Name'],type(dictionary))
