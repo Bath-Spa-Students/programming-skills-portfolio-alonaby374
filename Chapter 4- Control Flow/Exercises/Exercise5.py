518a4015c01fb93c30b25f14a43020effd63ff6d
alonaby374 <alonaby374@gmail.com>
2023-11-04 18:19:56 +0400

    Chapter 4

---
+++ Chapter 4- Control Flow/Exercises/Exercise5.py
@@ -0,0 +1,12 @@
+favorite_fruits = ['Mangos', 'Strawberry', 'Pineapple']
+
+if 'Mangos' in favorite_fruits:
+    print("You really like Mangos!")
+if 'Bananas' in favorite_fruits:
+    print("You really like Bananas!")
+if 'Strawberry' in favorite_fruits:
+    print("You really like Strawberry!")
+if 'Apple' in favorite_fruits:
+    print("You really like Apple!")
+if 'Pineapple' in favorite_fruits:
+    print("You really like Pineapple!")
