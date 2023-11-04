bd3fdc707d3d9dbd569b483fb596e54264b031fd
alonaby374 <alonaby374@gmail.com>
2023-11-04 18:20:58 +0400

    Update Exercise6.py

--- Chapter 3- Data Structures/Exercises/Exercise6.py
+++ Chapter 3- Data Structures/Exercises/Exercise6.py
@@ -24,13 +24,10 @@ print(f"{name}, please come have dinner with me.")
 name = guests[2]
 print(f"{name}, please come have dinner with me.")
 
-
-
 print("\nSorry, we can only invite two people to dinner.")
 
 name = guests.pop()
 print(f"Sorry, {name} there's no room at the table.")
-
 
 name = guests[0].title()
 print(f"{name}, please come to dinner.")
