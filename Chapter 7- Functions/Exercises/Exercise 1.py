9bd9cd74af9f725a586d21760369e75fcba98b15
alonaby374 <alonaby374@gmail.com>
2023-11-10 18:45:50 +0400

    Chapter 7

---
+++ Chapter 7- Functions/Exercises/Exercise 1.py
@@ -0,0 +1,5 @@
+def display_message():
+    m = "I'm learning to store code in functions."
+    print(m)
+
+display_message()
---
+++ Chapter 7- Functions/Exercises/Exercise 3.py
@@ -0,0 +1,6 @@
+def make_shirt(size, message):
+    print(f"\nI'm going to make a {size} t-shirt.")
+    print(f'It will say, "{message}"')
+
+make_shirt('medium', 'The best is yet to come.')
+make_shirt(message="You only live once, but if you do it right, once is enough.", size='small')
